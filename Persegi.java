e86416dcc5bafa87b2e523c6dc9c1f1491c3be5c
ivanrizalahmadi <143978436+ivanrizalahmadi@users.noreply.github.com>
2023-09-18 22:26:30 +0700

    Add files via upload

---
+++ Persegi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Persegi {
+        public static void main(String[] args) {
+            Scanner input =new Scanner(System.in);
+    
+            double panjang;
+            double luas;
+            double sisi;
+            double keliling;
+    
+            panjang = input.nextInt();
+    
+            luas = panjang * luas;
+            keliling = sisi * 4;
+    
+            System.out.println("Luas: " +luas);
+            System.out.println("Keliling: " +keliling);
+    
+        }
+    } 
+
