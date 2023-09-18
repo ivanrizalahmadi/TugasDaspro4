cd016a0782e565cd79c12dc0b186c2aac492470b
ivanrizalahmadi <143978436+ivanrizalahmadi@users.noreply.github.com>
2023-09-18 22:12:44 +0700

    Add files via upload

---
+++ Segitiga1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Segitiga1 {
+    public static voin main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        double alas;
+        double tinggi;
+        double sisi;
+        double luas;
+        double keliling;
+
+         alas = input.nextInt();
+
+        luas = alas * tinggi /2
+        keliling = sisi  + sisi  + sisi 
+
+        System.out.println("Luas: " +luas);
+        System.out.println("Keliling: " +keliling);
+
+    }
+
+   
+    
+}
